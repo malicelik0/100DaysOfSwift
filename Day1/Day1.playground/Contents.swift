e378825b5caae615465c95e5d373a0b9e69795ad
malicelik0 <malicelik123@gmail.com>
2022-06-29 21:22:37 +0300

    Day1

---
+++ Day1/Day1.playground/Contents.swift
@@ -0,0 +1,48 @@
+import UIKit
+// Variables
+
+var greeting = "Hello, playground"
+greeting = "GoodBye"
+
+// Strings & Integers
+
+var str = "Hello, playground"
+var age = 22
+var population = 8_000_000
+
+// Multi-line Strings
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// Doubles and booleans
+
+var pi = 3.141
+var awesome = true
+
+// String interpolation
+
+var score = 85
+var stri = "Your score was \(score)"
+var results = "The test results are here: \(stri)"
+
+// Constants
+
+let taylor = "swift"
+
+// Type annotations
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+
