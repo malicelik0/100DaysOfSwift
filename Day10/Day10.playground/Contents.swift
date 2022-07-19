135262c22fbbb24bb3a7883e9bfe00eb5081f388
malicelik0 <malicelik123@gmail.com>
2022-07-19 15:54:30 +0300

    Day10

---
+++ Day10/Day10.playground/Contents.swift
@@ -0,0 +1,101 @@
+import UIKit
+
+// Creating your own classes
+
+class Dog {
+    var name: String
+    var breed: String
+
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+
+let poppy = Dog(name: "Poppy", breed: "Poodle")
+
+// Class inheritance
+
+class Poodle: Dog {
+    init(name: String) {
+        super.init(name: name, breed: "Poodle")
+    }
+}
+
+// Overriding methods
+
+
+class Dog2 {
+    func makeNoise() {
+        print("Woof!")
+    }
+}
+
+class Poodle2: Dog2 {
+    override func makeNoise() {
+        print("Yip!")
+    }
+}
+
+let puppy = Poodle2()
+puppy.makeNoise()
+
+// Final classes
+
+final class Dog3 {
+    var name: String
+    var breed: String
+
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+
+// Copying objects
+
+class Singer {
+    var name = "Taylor Swift"
+}
+
+var singer = Singer()
+print(singer.name)
+
+var singerCopy = singer
+singerCopy.name = "Justin Bieber"
+
+print(singer.name)
+
+// Deinitializers
+
+class Person {
+    var name = "John Doe"
+
+    init() {
+        print("\(name) is alive!")
+    }
+
+    func printGreeting() {
+        print("Hello, I'm \(name)")
+    }
+    
+    deinit {
+        print("\(name) is no more!")
+    }
+    
+}
+
+for _ in 1...3 {
+    let person = Person()
+    person.printGreeting()
+}
+
+// Mutability
+
+class Singer2 {
+    var name = "Taylor Swift"
+}
+
+let taylor = Singer2()
+taylor.name = "Ed Sheeran"
+print(taylor.name)
